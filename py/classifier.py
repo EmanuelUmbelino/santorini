36ae11fedfeb6b258332708b1746717cb621aa61
PaMeirelles <rafaelpmeirelles@gmail.com>
2024-09-29 13:50:02 -0300

    Fixing issue with board validation

--- py/classifier.py
+++ py/classifier.py
@@ -4,42 +4,39 @@ from tkinter import filedialog
 from PIL import Image
 import json
 from datetime import datetime
+from constants import *
 
-image_path =  None
-json_path = None
-output_folder = None
-
-if image_path is None:
+if IMAGE_PATH is None:
   input('1º: Select the image you want to classify')
 
   root = tk.Tk()
   root.withdraw()
 
-  image_path = filedialog.askopenfilename()
+  IMAGE_PATH = filedialog.askopenfilename()
   print('Image path:')
-  print(image_path)
+  print(IMAGE_PATH)
 
   print()
   print()
 
-if json_path is None:
+if JSON_PATH is None:
   input('2º: Select the Json to classify')
 
-  json_path = filedialog.askopenfilename()
+  JSON_PATH = filedialog.askopenfilename()
   print('Json path:')
-  print(json_path)
+  print(JSON_PATH)
 
   print()
   print()
 
-if output_folder is None:
+if OUTPUT_FOLDER is None:
   input('3º: Select the folder to save the classified images')
 
   directory_path = filedialog.askdirectory()
   print('Folder path:')
   print(directory_path)
 
-with open(json_path, 'r') as file:
+with open(JSON_PATH, 'r') as file:
   data = json.load(file)
 
 width = data['resized']['width']
@@ -57,7 +54,7 @@ width_margin = grid_width * 0.05
 height_margin = grid_height * 0.05
 
 
-im = Image.open(image_path).resize((width, height))
+im = Image.open(IMAGE_PATH).resize((width, height))
 
 board = im.crop((
   grid_left - width_margin,
@@ -65,7 +62,7 @@ board = im.crop((
   grid_width + grid_left + width_margin,
   grid_height + grid_top + height_margin
 ))
-board.save(output_folder + '/board.jpg')
+board.save(OUTPUT_FOLDER + '/board.jpg')
 
 square_w = grid_width / 5
 square_h = grid_height / 5
@@ -78,9 +75,9 @@ color_blue = 0
 for i in range(5):
   for j in range(5):
     color = classification[i][j][1]
-    if color == 'G':
+    if color == 'C':
         color_gray += 1
-    if color == 'B':
+    if color == 'A':
       color_blue += 1
 
 if color_gray != 2 or color_blue != 2:
@@ -95,7 +92,7 @@ for i in range(5):
       grid_left + ((j+1) * square_w) + square_mw,
       grid_top + ((i+1) * square_h) + square_mh,
     ))
-    newPath = output_folder + '/' + classification[i][j]
+    newPath = OUTPUT_FOLDER + '/' + classification[i][j]
     if not os.path.exists(newPath):
       os.mkdir(newPath)
       now = datetime.now()
